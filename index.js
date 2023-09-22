14d91a4b56095316f9bb5c6c3bfc5f4b9c3323bd
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-09-22 13:01:52 +0600

    backend initial get/post structure done

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 4000;
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Bikroy.com backend');
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+client.connect(err => {
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to Bikroy.com backend');
+  })
+
+  // app.get('/profileInfo', (req, res) => {
+  // read data from here
+  // })
+
+  app.post('/profileInfo', (req, res) => {
+  // write data from here
+  })
 });
 
 app.listen(port, () => {
