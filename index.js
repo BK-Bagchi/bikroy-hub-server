a6bce5144960195eb60eed8ffdb623ec0c18196d
BK-Bagchi <bkbagchi.dipto@gmail.com>
2023-12-14 16:37:18 +0600

    sslcommrez intrigated and redirecting to payment getway

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+const SSLCommerzPayment = require('sslcommerz-lts')
+const store_id = process.env.SSL_STORE_ID
+const store_passwd = process.env.SSL_STORE_PASSWORD
+const is_live = false //true for live, false for sandbox
+
 
 async function run() {
   try {
@@ -131,12 +136,52 @@ async function run() {
           .then(result => res.send(result))
         })
       
+      const transactionId= new ObjectId().toString();
       app.post('/orderNow', async (req, res) => {
         const objectId = new ObjectId(req.body._id);
         const product= await postAds.findOne({_id: objectId});
         const orderInfo= req.body
         // console.log(product);
         
+        const data = {
+          total_amount: product.price,
+          currency: 'BDT',
+          tran_id: orderInfo._id, // use unique tran_id for each api call
+          success_url: 'http://localhost:3030/success',
+          fail_url: 'http://localhost:3030/fail',
+          cancel_url: 'http://localhost:3030/cancel',
+          ipn_url: 'http://localhost:3030/ipn',
+          shipping_method: 'Courier',
+          product_name: orderInfo.itemName,
+          product_category: orderInfo.category,
+          product_profile: 'general',
+          cus_name: orderInfo.userName,
+          cus_email: orderInfo.userEmail,
+          cus_add1: orderInfo.shippingAddress,
+          cus_add2: 'Dhaka',
+          cus_city: 'Dhaka',
+          cus_state: 'Dhaka',
+          cus_postcode: orderInfo.postCode,
+          cus_country: 'Bangladesh',
+          cus_phone: orderInfo.contactNumber,
+          cus_fax: '01711111111',
+          ship_name: orderInfo.userName,
+          ship_add1: orderInfo.shippingAddress,
+          ship_add2: 'Dhaka',
+          ship_city: 'Dhaka',
+          ship_state: 'Dhaka',
+          ship_postcode: orderInfo.postCode,
+          ship_country: 'Bangladesh',
+        };
+        // console.log(data);
+
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+        sslcz.init(data).then(apiResponse => {
+            // Redirect the user to payment gateway
+            let GatewayPageURL = apiResponse.GatewayPageURL
+            res.send({url: GatewayPageURL})
+            console.log('Redirecting to: ', GatewayPageURL)
+        });
       })
 
   } finally {
