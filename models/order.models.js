4e533dc846b83ac521005be081cf29de28276970
unknown <bkbagchi.dipto@gmail.com>
2025-09-09 17:10:38 +0600

    Added dispute figure and diff order status in orderInfo model

--- models/order.models.js
+++ models/order.models.js
@@ -1,11 +1,44 @@
 import mongoose from "mongoose";
+
+const disputeSchema = new mongoose.Schema(
+  {
+    isReported: { type: Boolean, default: false },
+    reportedBy: { type: String, enum: ["buyer", "seller"] },
+    reason: { type: String, trim: true },
+    status: { type: String, enum: ["pending", "resolved"], default: "pending" },
+    resolution: { type: String },
+    resolvedAt: { type: Date },
+  },
+  { _id: false }
+);
 const orderSchema = new mongoose.Schema(
   {
     orderId: { type: String },
-    productId: { type: mongoose.Schema.Types.ObjectId },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "addsInfo" },
     sellerEmail: { type: String },
     customerCredentials: { type: Object },
     paymentStatus: { type: Boolean, default: false },
+    orderStatusByAdmin: {
+      type: String,
+      enum: ["running", "aborted"],
+      default: "running",
+    },
+    orderStatusByBuyer: {
+      type: String,
+      enum: ["ordered", "cancelled"],
+      default: "ordered",
+    },
+    orderStatusBySeller: {
+      type: String,
+      enum: ["pending", "accepted", "cancelled"],
+      efault: "pending",
+    },
+    shipmentStatus: {
+      type: String,
+      enum: ["pending", "shipped", "delivered"],
+      default: "pending",
+    },
+    dispute: disputeSchema,
   },
   { timestamps: true }
 );
