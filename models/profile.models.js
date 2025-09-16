3cab47ba07262812409bb37a67aae93acdd0504d
unknown <bkbagchi.dipto@gmail.com>
2025-09-16 11:43:59 +0600

    profile model updated

--- models/profile.models.js
+++ models/profile.models.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const profileSchema = new mongoose.Schema(
   {
-    aboutBusiness: { type: String },
-    businessName: { type: String },
     displayName: { type: String },
     email: { type: String },
     phoneNumber: { type: String },
     photoURL: { type: String },
+    role: { type: String, enum: ["buyer", "seller", "admin"] },
+    paymentMedia: {
+      method: { type: String, enum: ["bank", "bkash", "rocket", "nagad"] },
+      accountNumber: { type: String },
+    },
+    favoriteAdds: [{ type: mongoose.Schema.Types.ObjectId, ref: "addsInfo" }],
   },
   { timestamps: true }
 );
